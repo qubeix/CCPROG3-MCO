9af28bfb3305ddb368eacf82ca82780f8df63084
qubeix <bq9906@gmail.com>
2026-06-12 17:49:17 +0800

    Added methods

--- Series.java
+++ Series.java
@@ -10,8 +10,8 @@ public class Series
     private final String TITLE;
     private final String GENRE;
 
-    private int seasonCount;     //the number of seasons
-    private int episodeCount;    //the number of episodes
+    private final int SEASONCOUNT;     //the number of seasons
+    private final int EPISODECOUNT;    //the number of episodes
     private int currentSeason;         //the user's current season
     private int currentEpisode;        //the user's current episode
 
@@ -21,15 +21,8 @@ public class Series
         TITLE = title;
         GENRE = genre;
 
-        if(seasonCount > 0)
-            this.seasonCount = seasonCount;
-        else
-            System.out.println("Invalid Seasons");
-
-        if(episodeCount > 0)
-            this.episodeCount = episodeCount;
-        else
-            System.out.println("Invalid Episodes");
+        SEASONCOUNT = seasonCount;
+        EPISODECOUNT = episodeCount;
 
         //Initialization
         status = "Planned";
@@ -37,5 +30,75 @@ public class Series
         review = "";
         currentSeason = 0;
         currentEpisode = 0;
+    }
+
+    //Methods
+    public void addStatus(String status)
+    {
+        if(status.equals("Planned") || status.equals("In Progress") || status.equals("Completed"))  //If the status is valid
+            this.status = status;  //Assign the new status
+        else                                       //If the new status is not valid
+            System.out.println("Invalid status");  //Display a message
+    }
+
+    public void updateStatus(String newStatus)
+    {
+        if(newStatus.equals("Planned") || newStatus.equals("In Progress") || newStatus.equals("Completed"))  //If the new status is valid
+            this.status = newStatus;  //Assign the new status
+        else                                       //If the new status is not valid
+            System.out.println("Invalid status");  //Display a message
+    }
+
+    public void addRating(int rating)
+    {
+        if(status.equals("Completed"))  //If the entry's status is "Completed"
+            this.rating = rating;       //Assign the rating
+        else                                                        //If the entry's status is not "Completed"
+            System.out.println("This entry is yet to completed");  //Display a message
+    }
+
+    public void addReview(String review)
+    {
+        if(status.equals("Completed"))  //If the entry's status is "Completed"
+            this.review = review;       //Assign the review
+        else                                                        //If the entry's status is not "Completed"
+            System.out.println("This entry is yet to completed");  //Display a message
+    }
+
+    public void nextEpisode()
+    {
+        if(currentEpisode < EPISODECOUNT)   //If the current episode has not reached the last episode
+        {
+            currentEpisode++;                   //Move to the next chapter
+
+            if(status.equals("Planned"))           //If the status if "Planned"
+                updateStatus("In Progress");       //Update the status to "In Progress"
+            
+            if(currentEpisode == episodeCount)     //If the current chapter is the last chapter
+                updateStatus("Completed");         //Update the status to "Completed"
+        }
+        else                                                                //If the book is completed
+            System.out.println("You have already completed this book");    //Display a message
+    }
+
+    //Getters
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public int getRating()
+    {
+        return rating;
+    }
+
+    public String getReview()
+    {
+        return review;
+    }
+
+    public String getProgress()
+    {
+        return "Season " + currentSeason + ": " + currentEpisode + "/" + EPISODECOUNT + " Episodes";   //return the amount of chapters read
     }
 }
