02bcae4c73f0916a9e8c02d720857d9e4d211ffd
qubeix <bq9906@gmail.com>
2026-07-10 18:50:32 +0800

    Added back import (oops)

--- Series.java
+++ Series.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
   * The Series class contains the attributes, constructor, methods, and getters necessary for the creation, modification, and manipulation of a series entry with:
   * the common methods of: assigning a status (and modifying it), assigning a rating, and assigning a review; and
