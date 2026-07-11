716a577b6abe0c19df5b241f911b6068fbea79d4
qubeix <bq9906@gmail.com>
2026-07-11 10:57:53 +0800

    Edited formatting of javadoc documentation

--- Series.java
+++ Series.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 /**
-  * The Series class contains the attributes, constructor, methods, and getters necessary for the creation, modification, and manipulation of a series entry with:
-  * the common methods of: assigning a status (and modifying it), assigning a rating, and assigning a review; and
+  * The class <code>Series</code> contains the attributes, constructor, methods, and getters necessary for the creation, modification, and manipulation of a series entry with:<br>
+  * the common methods of: assigning a status (and modifying it), assigning a rating, and assigning a review; and<br>
   * the unique methods of: assigning the number of episodes per season, and updating the series entry's current episode and season progress
  */
 public class Series
@@ -21,6 +21,12 @@ public class Series
     private int currentEpisode;        //the user's current episode
 
     //CONSTRUCTOR
+    /**
+      * Accepts a title, genre, and count of seasons as parameters, and initializes status to "Plenned," rating to 0, review to empty, current season to 1, and current episode to 0, and list of episode counts 
+      * @param title the title of the series
+      * @param genre the genre of the series
+      * @param seasonCount the count of seasons in the series
+     */
     public Series(String title, String genre, int seasonCount)
     {
         TITLE = title;
@@ -40,8 +46,8 @@ public class Series
 
     //METHODS
     /**
-      * addStatus() assigns a status ("Planned", "In Progress", or "Completed") to an entry
-      * @param status - the status to be assigned
+      * Assigns a status ("Planned", "In Progress", or "Completed") to an entry
+      * @param status the status to be assigned
      */
     public void addStatus(String status)
     {
@@ -52,8 +58,8 @@ public class Series
     }
     
     /**
-      * updateStatus() modifies an entry's status ("Planned", "In Progress", or "Completed")
-      * @param newStatus - the status to be newly assigned
+      * Modifies an entry's status ("Planned", "In Progress", or "Completed")
+      * @param newStatus the status to be newly assigned
      */
     public void updateStatus(String newStatus)
     {
@@ -70,9 +76,9 @@ public class Series
     }
 
     /**
-      * addRating() assigns a rating to an entry
-      * @param rating - the number rating to be assigned
-      * @pre - the entry must have a status of "Completed" to assign a rating
+      * Assigns a rating to an entry
+      * @param rating the number rating to be assigned
+      * @pre the entry must have a status of "Completed" to assign a rating
      */
     public void addRating(int rating)
     {
@@ -83,9 +89,9 @@ public class Series
     }
 
     /**
-      * addReview() assigns a review to an entry
-      * @param review - the review to be assigned
-      * @pre - the entry must have a status of "Completed" to assign a review
+      * Assigns a review to an entry
+      * @param review the review to be assigned
+      * @pre the entry must have a status of "Completed" to assign a review
      */
     public void addReview(String review)
     {
@@ -96,7 +102,7 @@ public class Series
     }
 
     /**
-      * addEpisodes() assigns a number of episodes per season
+      * Assigns a number of episodes per season
      */
     public void addEpisodes()
     {
@@ -112,7 +118,7 @@ public class Series
     }
 
     /**
-      * nextEpisode() moves the current episode and current season forward by one as necessary
+      * Moves the current episode and current season forward by one as necessary
      */
     public void nextEpisode()
     {
@@ -136,41 +142,73 @@ public class Series
     }
 
     //GETTERS
+    /**
+   	  * Returns the status of the series
+   	  * @return the status
+   	 */
     public String getStatus()
     {
         return status;
     }
 
+    /**
+   	  * Returns the rating of the series
+   	  * @return the rating
+   	 */
     public int getRating()
     {
         return rating;
     }
 
+    /**
+   	  * Returns the review of the series
+   	  * @return the review
+   	 */
     public String getReview()
     {
         return review;
     }
 
+    /**
+   	  * Returns the title of the series
+   	  * @return the title
+   	 */
     public String getTitle()
     {
         return TITLE;
     }
 
+    /**
+   	  * Returns the genre of the series
+   	  * @return the genre
+   	 */
     public String getGenre()
     {
         return GENRE;
     }
 
+    /**
+   	  * Returns the number of seasons in the series
+   	  * @return the number of seasons
+   	 */
     public int getSeasonCount()
     {
         return SEASONCOUNT;
     }
 
+    /**
+   	  * Returns the number of episodes of a given season in the series
+   	  * @return the number of episodes of a given season
+   	 */
     public int getEpisodeCount(int season)
     {
         return episodeCount[season-1];
     }
 
+    /**
+   	  * Returns the number of episodes in the series
+   	  * @return the number of episodes
+   	 */
     public int getEpisodeCount(){
         int total = 0;
         for (int i = 0; i < SEASONCOUNT; i++){
@@ -179,6 +217,10 @@ public class Series
         return total; 
     }
 
+    /**
+   	  * Returns the progress of the series, in the format of: "Season <current season>: <current episode>/<total episodes in current season> Episodes"
+   	  * @return the progress
+   	 */
     public String getProgress()
     {
         return "Season " + currentSeason + ": " + currentEpisode + "/" + episodeCount[currentSeason-1] + " Episodes\n";   //return the season and amount of episodes watched
