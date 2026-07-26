bd00b17041e7dca62721e1d019f7426690201a62
qubeix <bq9906@gmail.com>
2026-07-26 13:19:10 +0800

    Add package

--- Book.java
+++ Book.java
@@ -1,3 +1,5 @@
+package com.example.model;
+
 /**
   * The class <code>Book</code> contains the attributes, constructor, methods, and getters necessary for the creation, modification, and manipulation of a book entry with:<br>
   * the inherited methods of: assigning a status (and modifying it), assigning a rating, and assigning a review; and<br>
